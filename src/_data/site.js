847588dad7fab82f03a040dee272799e45919083
Maher <kangmaheri@gmail.com>
2023-04-12 11:33:35 +0700

    first modified

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: 'Vredeburg',
-  description: 'Eleventy starter project to create a blog',
+  title: 'My Portfolio',
+  description: 'This is list of my Websites',
   keywords: ['eleventy', 'template', 'simple', 'clean'],
   url: 'https://vredeburg.netlify.app', // your site url without trailing slash
   paginate: 6 // how many posts you want to show for each page
